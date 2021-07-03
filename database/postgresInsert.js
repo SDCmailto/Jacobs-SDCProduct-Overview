2df8f9f51f919c2b9526d34779bd686878362646
Jacob <ja.metevia@gmail.com>
2021-07-02 18:38:27 -0700

    finished seeding script and inserting data to postgres

--- database/postgresInsert.js
+++ database/postgresInsert.js
@@ -1,12 +1,8 @@
 const { Pool, Client } = require("pg");
+const config = require('../config.js');
 
 
-const pool = new Pool({
-  user: "jametevia",
-  host: "localhost",
-  database: "amazon_products",
-  port: "5432"
-});
+const pool = new Pool(config);
 
 
 //do this first because of foreign key constraints
@@ -14,7 +10,7 @@ const pool = new Pool({
 
 let sellerQuery = `
 COPY other_sellers(id, discs, price, newfrom, usedfrom, edition, form, release_date)
-FROM '/Users/jametevia/rpt/ProductOverview/sellers.csv'
+FROM '/Users/jametevia/rpt/ProductOverview/sellers13.csv'
 DELIMITER ','
 CSV HEADER;`;
 
@@ -22,10 +18,14 @@ CSV HEADER;`;
 pool.query(
   sellerQuery,
   (err, res) => {
-    console.log(err, res);
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('copied ' + res.rowCount + ' seller entries');
+    }
     let productQuery = `
 COPY products(id, product_id, product_name, package_name, list_price, price, prime, sold_by, ships_from, in_stock, inventory)
-FROM '/Users/jametevia/rpt/ProductOverview/products.csv'
+FROM '/Users/jametevia/rpt/ProductOverview/products13.csv'
 DELIMITER ','
 CSV HEADER;`;
 
@@ -33,10 +33,14 @@ CSV HEADER;`;
     pool.query(
       productQuery,
       (err, res) => {
-        console.log(err, res);
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('copied ' + res.rowCount + ' product entries');
+        }
         let formsQuery = `
 COPY forms(id, price, form, id_products_foreign, product_id)
-FROM '/Users/jametevia/rpt/ProductOverview/forms.csv'
+FROM '/Users/jametevia/rpt/ProductOverview/forms13.csv'
 DELIMITER ','
 CSV HEADER;`;
 
@@ -44,10 +48,14 @@ CSV HEADER;`;
         pool.query(
           formsQuery,
           (err, res) => {
-            console.log(err, res);
+            if (err) {
+              console.log(err)
+            } else {
+              console.log('copied ' + res.rowCount + ' form entries');
+            }
             let products_other_sellers_table_data_query = `
 COPY products_and_other_sellers(id, id_products_foreign, id_other_sellers_foreign, product_id)
-FROM '/Users/jametevia/rpt/ProductOverview/products_other_sellers_table_data.csv'
+FROM '/Users/jametevia/rpt/ProductOverview/products_other_sellers_table_data13.csv'
 DELIMITER ','
 CSV HEADER;`;
 
@@ -55,7 +63,11 @@ CSV HEADER;`;
             pool.query(
               products_other_sellers_table_data_query,
               (err, res) => {
-                console.log(err, res);
+                if (err) {
+                  console.log(err)
+                } else {
+                  console.log('copied ' + res.rowCount + ' product_and_other_sellers entries');
+                }
                 pool.end();
               }
             );
